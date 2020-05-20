7f4074f51380c686e9245c0f78a08c27ab3cdd78
Alex <budnikoleksii@gmail.com>
2020-05-20 17:54:34 +0300

    form-calc was added

--- Window/src/js/modules/forms.js
+++ Window/src/js/modules/forms.js
@@ -1,13 +1,10 @@
-const forms = () => {
-    const form = document.querySelectorAll('form'),
-          inputs = document.querySelectorAll('input'),
-          phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+import checkNumInputs from './checkNumInputs';
 
-    phoneInputs.forEach(item => {
-        item.addEventListener('input', () => {
-            item.value = item.value.replace(/\D/, '');
-        });
-    });
+const forms = (state) => {
+    const form = document.querySelectorAll('form'),
+          inputs = document.querySelectorAll('input');
+
+    checkNumInputs('input[name="user_phone"]');
 
     const message = {
         loading: 'Загрузка...',
@@ -41,6 +38,11 @@ const forms = () => {
             item.appendChild(statusMessage);
 
             const formData = new FormData(item);
+            if (item.getAttribute('data-calc') === "end") {
+                for (let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
 
             postData('assets/server.php', formData)
                 .then(res => {
@@ -53,6 +55,14 @@ const forms = () => {
                     setTimeout(() => {
                         statusMessage.remove();
                     }, 5000);
+
+                    // close end modal after finish
+                    if (item.getAttribute('data-calc') === "end") {
+                        setTimeout(() => {
+                            item.closest('.popup_calc_end').style.display = "none";
+                            document.body.style.overflow = "scroll";
+                        }, 6000);
+                    }
                 });
         });
     });
