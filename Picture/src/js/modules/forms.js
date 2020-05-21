7373f1d6fc7156ffd80d581bc69d44070d08995f
Alex <budnikoleksii@gmail.com>
2020-05-21 18:45:57 +0300

    added cards from json db

--- Picture/src/js/modules/forms.js
+++ Picture/src/js/modules/forms.js
@@ -1,4 +1,4 @@
-// import checkNumInputs from './checkNumInputs';
+import {postData} from '../services/requests';
 
 const forms = () => {
     const form = document.querySelectorAll('form'),
@@ -19,16 +19,6 @@ const forms = () => {
         question: 'assets/question.php'
     };
     
-    // await for fetch cause we need to wait for result from promise
-    const postData = async (url, data) => {
-        let res = await fetch(url, {
-            method: "POST",
-            body: data
-        });
-
-        return await res.text();
-    };
-
     const clearInputs = () => {
         inputs.forEach(item => {
             item.value = '';
