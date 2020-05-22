f22f4e5e747f8a0835de2a7bff56ff6dd8d005d1
Alex <budnikoleksii@gmail.com>
2020-05-22 05:56:15 +0300

    the calc was added

---
+++ Picture/src/js/modules/calc.js
@@ -0,0 +1,28 @@
+const calc = (size, material, options, promocode, result) => {
+    const sizeBlock = document.querySelector(size),
+          materialBlock = document.querySelector(material),
+          optionsBlock = document.querySelector(options),
+          promocodelBlock = document.querySelector(promocode),
+          resultBlock = document.querySelector(result);
+
+    let sum = 0;
+
+    const calcFunction = () => {
+        sum = Math.round((+sizeBlock.value) * (+materialBlock.value) + (+optionsBlock.value));
+
+        if (sizeBlock.value == '' || materialBlock.value == '') {
+            resultBlock.textContent = "Пожалуйста, выберете размер и материал картины";
+        } else if (promocodelBlock.value === 'IWANTPOPART') {
+            resultBlock.textContent = Math.round(sum * 0.7);
+        } else {
+            resultBlock.textContent = sum;
+        }
+    };
+
+    sizeBlock.addEventListener('change', calcFunction);
+    materialBlock.addEventListener('change', calcFunction);
+    optionsBlock.addEventListener('change', calcFunction);
+    promocodelBlock.addEventListener('input', calcFunction);
+};
+
+export default calc;
