faf9ea2b1a3a6c443fc5d9c71bb30c4cb456c63a
Alex <budnikoleksii@gmail.com>
2020-07-18 21:42:44 +0300

    refactored filter module

--- Picture/src/js/modules/filter.js
+++ Picture/src/js/modules/filter.js
@@ -1,77 +1,49 @@
 const filter = () => {
-    const menu = document.querySelector('.portfolio-menu'),
-          items = menu.querySelectorAll('li'),
-          btnAll = menu.querySelector('.all'),
-          btnLovers = menu.querySelector('.lovers'),
-          btnChef = menu.querySelector('.chef'),
-          btnGirl = menu.querySelector('.girl'),
-          btnGuy = menu.querySelector('.guy'),
-          btnGrandmother = menu.querySelector('.grandmother'),
-          btnGranddad = menu.querySelector('.granddad'),
-          wrapper = document.querySelector('.portfolio-wrapper'),
-          markAll = wrapper.querySelectorAll('.all'),
-          markGirl = wrapper.querySelectorAll('.girl'),
-          markLovers = wrapper.querySelectorAll('.lovers'),
-          markChef = wrapper.querySelectorAll('.chef'),
-          markGuy = wrapper.querySelectorAll('.guy'),
-          no = document.querySelector('.portfolio-no');
+	const menu = document.querySelector('.portfolio-menu'),
+		items = menu.querySelectorAll('li'),
+		wrapper = document.querySelector('.portfolio-wrapper'),
+		markAll = wrapper.querySelectorAll('.all'),
+		no = document.querySelector('.portfolio-no');
 
-    const typeFilter = (markType) => {
-        markAll.forEach(mark => {
-            mark.style.display = 'none';
-            mark.classList.remove('animated', 'fadeIn');
-        });
+		markAll.forEach(mark => {
+			mark.style.display = 'block';
+			mark.classList.add('animated');
+			mark.classList.remove('fadeIn');
+		});
 
-        no.style.display = "none";
-        no.classList.remove('animated', 'fadeIn');
+		no.style.display = 'none';
+		no.classList.add('animated');
+		no.classList.remove('fadeIn');
+	
+	menu.addEventListener('click', (e) => {
+		let target = e.target;
+		
+		if (target && target.tagName == "LI") {
+	
+			markAll.forEach(mark => {
+				mark.style.display = 'none';
+				mark.classList.remove('fadeIn');
+			});
+	
+			no.style.display = 'none';
+			no.classList.remove('fadeIn');
 
-        if (markType) {
-            markType.forEach(mark => {
-                mark.style.display = 'block';
-                mark.classList.add('animated', 'fadeIn');
-            });
-        } else {
-            no.style.display = 'block';
-            no.classList.add('animated', 'fadeIn');
-        }
-    };
+			let classBtn = target.classList.value;
 
-    btnAll.addEventListener('click', () => {
-        typeFilter(markAll);
-    });
+			items.forEach(btn => btn.classList.remove('active'));
+			target.classList.add('active');
 
-    btnLovers.addEventListener('click', () => {
-        typeFilter(markLovers);
-    });
-
-    btnChef.addEventListener('click', () => {
-        typeFilter(markChef);
-    });
-
-    btnGuy.addEventListener('click', () => {
-        typeFilter(markGuy);
-    });
-
-    btnGirl.addEventListener('click', () => {
-        typeFilter(markGirl);
-    });
-
-    btnGrandmother.addEventListener('click', () => {
-        typeFilter();
-    });
-
-    btnGranddad.addEventListener('click', () => {
-        typeFilter();
-    });
-
-    menu.addEventListener('click', (e) => {
-        let target = e.target;
-
-        if (target && target.tagName == "LI") {
-            items.forEach(btn => btn.classList.remove('active'));
-            target.classList.add('active');
-        }
-    });
+			markAll.forEach(mark => {
+				if (mark.classList.contains(classBtn)) {
+					mark.style.display = 'block';
+					mark.classList.add('fadeIn');
+				} else if (classBtn == 'grandmother' || classBtn == 'granddad') {
+					no.style.display = 'block';
+					no.classList.add('fadeIn');
+				}
+			});
+		}
+	});
 };
 
 export default filter;
