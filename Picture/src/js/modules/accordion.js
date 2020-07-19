93752df912a79bfaa71a61a425633cdedfd57d85
Alex <budnikoleksii@gmail.com>
2020-07-19 20:46:13 +0300

    updated collapse elements

--- Picture/src/js/modules/accordion.js
+++ Picture/src/js/modules/accordion.js
@@ -1,39 +1,19 @@
 const accordion = (triggersSelector) => {
-    const btns = document.querySelectorAll(triggersSelector);
+	const btns = document.querySelectorAll(triggersSelector);
 
-    btns.forEach(btn => {
-        btn.addEventListener('click', function() {
-            // btns.forEach(btn => {
-            //     btn.classList.remove('active-style');
-            //     btn.nextElementSibling.classList.remove('active-content');
-            //     btn.nextElementSibling.style.maxHeight = 0 + 'px';
-            //  });
-            this.classList.toggle('active-style');
-            this.nextElementSibling.classList.toggle('active-content');
+	btns.forEach(btn => {
+		btn.addEventListener('click', function() {
+			if (!this.classList.contains('active-style')) {
+				btns.forEach(btn => {
+					btn.classList.remove('active-style');
+					btn.nextElementSibling.classList.remove('active-content');
+				});
+			}
 
-            if (this.classList.contains('active-style')) {
-                this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + 'px';
-            } else {
-                this.nextElementSibling.style.maxHeight = '0px';
-            }
-        });
-    });
-        //   blocks = document.querySelectorAll(itemsSelector);
-
-    // blocks.forEach(block => {
-    //     block.classList.add('animated', 'fadeInDown');
-    // });
-
-    // btns.forEach(btn => {
-    //     btn.addEventListener('click', function() {
-    //         if (!this.classList.contains('active')) {
-    //             btns.forEach(btn => {
-    //                 btn.classList.remove('active', 'active-style');
-    //             });
-    //             this.classList.add('active', 'active-style');
-    //         }
-    //     });
-    // });
+			this.classList.toggle('active-style');
+			this.nextElementSibling.classList.toggle('active-content');
+		});
+	});
 };
 
 export default accordion;
