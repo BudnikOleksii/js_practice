1aaf324d84b1cdca09c91974a3d077ae3ae6c03d
Alex <budnikoleksii@gmail.com>
2020-07-25 19:53:10 +0300

    slide animations were added

--- Library/src/js/lib/modules/effects.js
+++ Library/src/js/lib/modules/effects.js
@@ -8,8 +8,8 @@ $.prototype.animateOverTime = function(dur, cb, fin) {
             timeStart = time;
         }
 
-        let timeElapsed = time - timeStart;
-        let complection = Math.min(timeElapsed / dur, 1);
+        let timeElapsed = time - timeStart,
+            complection = Math.min(timeElapsed / dur, 1);
 
         cb(complection);
 
@@ -33,8 +33,8 @@ $.prototype.fadeIn = function(dur, display, fin) {
             this[i].style.opacity = complection;
         };
 
-        const ani = this.animateOverTime(dur, _fadeIn, fin);
-        requestAnimationFrame(ani);
+        const animate = this.animateOverTime(dur, _fadeIn, fin);
+        requestAnimationFrame(animate);
     }
 
     return this;
@@ -55,4 +55,69 @@ $.prototype.fadeOut = function(dur, fin) {
     }
 
     return this;
+};
+
+$.prototype.fadeToggle = function(dur, display, fin) {
+	for (let i = 0; i < this.length; i++) {
+		if (window.getComputedStyle(this[i]).display === 'none') {
+			$(this[i]).fadeIn(dur, display, fin);
+		} else {
+			$(this[i]).fadeOut(dur, fin);
+		}
+	}
+
+	return this;
+};
+
+$.prototype.slideIn = function(dur, display, fin) {
+	for (let i = 0; i < this.length; i++) {
+		this[i].style.display = display || 'block';
+
+		const _slideIn = (complection) => {
+			let height = complection * this[i].scrollHeight;
+			this[i].style.height = height + 'px';
+			this[i].style.overflow = '';
+		};
+
+
+		const anim = this.animateOverTime(dur, _slideIn, fin);
+		requestAnimationFrame(anim);
+	}
+
+	return this;
+};
+
+$.prototype.slideOut = function(dur, fin) {
+	for (let i = 0; i < this.length; i++) {
+
+		const _slideOut = (complection) => {
+			this[i].style.overflow = 'hidden';
+			
+			let height = (1 - complection) * this[i].scrollHeight;
+			
+			this[i].style.height = height + 'px';
+
+			if (complection === 1) {
+				this[i].style.display = 'none';
+			}
+		};
+
+		const anim = this.animateOverTime(dur, _slideOut, fin);
+		requestAnimationFrame(anim);
+	}
+
+	return this;
+};
+
+$.prototype.slideToggle = function(dur, display, fin) {
+	for (let i = 0; i < this.length; i++) {
+		if (window.getComputedStyle(this[i]).display === 'none' || 
+			window.getComputedStyle(this[i]).height === '0px') {
+			$(this[i]).slideIn(dur, display, fin);
+		} else {
+			$(this[i]).slideOut(dur, fin);
+		}
+	}
+
+	return this;
 };
--- Library/src/js/main.js
+++ Library/src/js/main.js
@@ -8,12 +8,12 @@ $('div').click(function() {
     console.log($(this).index());
 });
 
-$('button').fadeIn(1800);
+$('button').slideIn(1800);
 
 // console.log($('div').eq(2).find('.some'));
 // console.log($('.some').closestElem('.findmeq').addClass('dgdg'));
 $('button').on('click', function() {
-    $(this).fadeOut(1800);
+    $(this).slideOut(1800);
 });
 
 // console.log($('div').html('hello'));
