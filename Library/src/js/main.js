bb094dcaab667f21f49016cf7cd6234019499592
Alex <budnikoleksii@gmail.com>
2020-07-25 18:46:59 +0300

    added closest and siblings method and fixed bug

--- Library/src/js/main.js
+++ Library/src/js/main.js
@@ -8,6 +8,8 @@ $('div').click(function() {
     console.log($(this).index());
 });
 
-console.log($('div').eq(2).find('.some'));
+// console.log($('div').eq(2).find('.some'));
+// console.log($('.some').closestElem('.findmeq').addClass('dgdg'));
+console.log($('.findme').siblings());
 
 // console.log($('div').html('hello'));
