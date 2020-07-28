4685eebe3c55366531889067c569bdd52561f075
Alex <budnikoleksii@gmail.com>
2020-07-28 09:06:39 +0300

    updates for modal script

--- Library/src/js/lib/components/modal.js
+++ Library/src/js/lib/components/modal.js
@@ -49,3 +49,53 @@ $.prototype.modal = function(created) {
 };
 
 $('[data-toggle="modal"]').modal();
+
+$.prototype.createModal = function({txtTitle, txtBody, btnCount, btnSettings} = {}) {
+	for (let i = 0; i < this.length; i++) {
+		let modal = document.createElement('div');
+		$(modal).addClass('modal');
+		$(modal).setAttr('id', $(this[i]).getAttr('data-target').slice(1));
+
+		const buttons = [];
+		for (let j = 0; j < btnCount; j++) {
+			let btn = document.createElement('button');
+			$(btn).addClass('btn', ...btnSettings[j][1]);
+			btn.textContent = btnSettings[j][0];
+			if (btnSettings[j][2]) {
+				$(btn).setAttr('data-close', 'true');
+            }
+            
+			if (btnSettings[j][3] && typeof(btnSettings[j][3]) === 'function') {
+				$(btn).click(btnSettings[j][3]);
+            }
+            
+			buttons.push(btn);
+		}
+
+		$(modal).html(
+			`<div class="modal-dialog">
+				<div class="modal-content">
+					<button class="close" data-close>
+						<span>&times;</span>
+					</button>
+					<div class="modal-header">
+						<div class="modal-title">
+							${txtTitle}
+						</div>
+					</div>
+					<div class="modal-body">
+						${txtBody}
+					</div>
+					<div class="modal-footer">
+						
+					</div>
+				</div>
+			</div>`
+		);
+
+		modal.querySelector(".modal-footer").append(...buttons);
+		document.body.appendChild(modal);
+		$(this[i]).modal(true);
+		$($(this[i]).getAttr('data-target')).fadeIn(300);
+	}
+};
--- Library/src/js/main.js
+++ Library/src/js/main.js
@@ -26,3 +26,34 @@ $('.wrap').html(
 );
 
 $('.dropdown-toggle').dropdown();
+
+$('#trigger').click(() => $('#trigger').createModal(
+	{
+		txtTitle: 'Modal title',
+		txtBody: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum minus doloremque nesciunt enim rem quam corporis? Dolorem pariatur magnam distinctio perferendis. Ratione dolorem voluptates iusto facilis odit veritatis, suscipit voluptatibus!',
+		btnCount: 3,
+		btnSettings: [
+			[
+				'Close',
+				['btn-danger', 'mr-10'],
+				true
+			],
+			[
+				'Save changes',
+				['btn-success'],
+				false,
+				() => {
+					alert('Данные сохранены');
+				}
+			],
+			[
+				'Exstra btn',
+				['btn-warning', 'ml-10'],
+				false,
+				() => {
+					alert('Hello World');
+				}
+			]
+		]
+	}
+));
