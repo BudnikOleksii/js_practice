df8a3535fe24277b90b3793a061025ea48d1d3ab
Alex <budnikoleksii@gmail.com>
2020-08-07 10:54:06 +0300

    prepared for work with autocomplite

--- Avia/src/js/store/locations.js
+++ Avia/src/js/store/locations.js
@@ -5,6 +5,7 @@ class Locations {
         this.api = api;
         this.countries = null;
         this.cities = null;
+        this.shortCitiesList = null;
     }
 
     async init() {
@@ -14,10 +15,43 @@ class Locations {
         ]);
 
         const [countries, cities] = response;
-        this.countries = countries;
-        this.cities = cities;
+        this.countries = this.serializeCountries(countries);
+        this.cities = this.serializeCities(cities);
+        this.shortCitiesList = this.createShortCitiesList(this.cities);
 
         return response;
+    }
+
+    createShortCitiesList(cities) {
+        // Object.entries => [key, value]
+        return Object.entries(cities).reduce((acc, [key]) => {
+            console.log(key);
+            acc[key] = null;
+            return acc;
+        }, {});
+    }
+
+    serializeCountries(countries) {
+        // { 'Country code: { ... } }
+        return countries.reduce((acc, country) => {
+            acc[country.code] = country;
+            return acc;
+        }, {});
+    }
+
+    serializeCities(cities) {
+        // { 'City name, country name': { ... } }
+        return cities.reduce((acc, city) => {
+            const country_name = this.getCountryNameByCode(city.country_code);
+            const city_name = city.name || city.name_translations.en;
+            const key = `${city_name},${country_name}`;
+            acc[key] = city;
+            return acc;
+        }, {});
+    }
+
+    getCountryNameByCode(code) {
+        return this.countries[code].name;
     }
 
     getCitiesByCountryCode(code) {
