d7348f5407144764b1afd60f5612d279dca0e699
Alex <budnikoleksii@gmail.com>
2020-08-13 23:17:42 +0300

    validation

---
+++ login/src/js/app.js
@@ -0,0 +1,29 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import '../css/style.css';
+
+import UI from './config/ui.config';
+import { validate } from './helpers/validate';
+// import { showInputError, removeInputError } from './views/form';
+// import { login } from './services/auth.service';
+// import { notify } from './views/notifications';
+// import { getNews } from './services/news.service';
+
+const { form, inputEmail, inputPassword } = UI;
+const inputs = [inputEmail, inputPassword];
+
+// Events
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    onSubmit();
+});
+
+// Handlers
+function onSubmit() {
+    const isValidForm = inputs.every((el) => {
+        const isValidInput = validate(el);
+
+        return isValidInput;
+    });
+
+    console.log(isValidForm);
+}
