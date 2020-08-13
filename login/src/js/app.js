3defce005a32d06f0ccd9f02118b69e4a39bbfba
Alex <budnikoleksii@gmail.com>
2020-08-13 23:29:45 +0300

    error template was added

--- login/src/js/app.js
+++ login/src/js/app.js
@@ -3,7 +3,7 @@ import '../css/style.css';
 
 import UI from './config/ui.config';
 import { validate } from './helpers/validate';
-// import { showInputError, removeInputError } from './views/form';
+import { showInputError, removeInputError } from './views/form';
 // import { login } from './services/auth.service';
 // import { notify } from './views/notifications';
 // import { getNews } from './services/news.service';
@@ -16,14 +16,16 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     onSubmit();
 });
+inputs.forEach(el => el.addEventListener('focus', () => removeInputError(el)));
 
 // Handlers
 function onSubmit() {
     const isValidForm = inputs.every((el) => {
         const isValidInput = validate(el);
+        if (!isValidInput) {
+            showInputError(el);
+        }
 
         return isValidInput;
     });
-
-    console.log(isValidForm);
 }
